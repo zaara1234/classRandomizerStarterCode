b4083f3d21c99fd99388621c752c69a4f725fa27
zaara afida <zafida.student@hsfi.us>
2017-03-21 21:22:06 +0000

    i made the code work.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-
-
-
+    var teachers = ["Nick", "Michael", "Mat", "Rafi", "Jenna"];
+    var student = ['pamela', 'lauren', 'felix', 'jacky', 'isabela', 'daril', 'ana', 'Zaara']
+ 
+   $("#studentButton").click(function(){
+    $("#studentDisplay").html(random2)
+          var random2 = Math.floor(Math.random()* student.length);
+              console.log(student[random2]);
+                  $("#studentDisplay").html(student[random2]);
+      });
+      $('#teacherButton').click(function(){
+    $("#teacherDisplay").html(random1)
+          var random1 = Math.floor(Math.random()* teachers.length);
+              console.log(student[random1]);
+                  $("#teacherDisplay").html(teachers[random1]);
+      });
 });
